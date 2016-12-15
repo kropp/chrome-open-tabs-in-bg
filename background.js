aa27fd7fba23b69e9c1660da189155c63061f115
Victor Kropp <victor.kropp@jetbrains.com>
2016-12-15 15:06:06 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,18 @@
+var lastOpenedTabId = undefined;
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    lastOpenedTabId = undefined;
+});
+
+chrome.tabs.onCreated.addListener(function (tab) {
+    if (tab.url != "chrome://newtab/" && tab.openerTabId) {
+        chrome.tabs.update(tab.openerTabId, {active: true}, function () {
+            lastOpenedTabId = tab.id;
+        });
+        if (lastOpenedTabId) {
+            chrome.tabs.get(lastOpenedTabId, function (lastOpenedTab) {
+                chrome.tabs.move(tab.id, {index: lastOpenedTab.index});
+            });
+        }
+    }
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+Open Tabs in Background
+=======================
+
+This extension makes Chrome behave like Firefox when opening new tabs:
+
+1. New tab always opens in background
+2. When you open multiple tabs in a row, every next one opens to the right of the last opened background tab.
+   By default they open to the right of the active tab, what I find inconvenient.
