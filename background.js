9f151c6c1b59b89dacc60fa8e09127a5ff1c10d4
Victor Kropp <victor.kropp@jetbrains.com>
2016-12-30 15:03:21 +0100

    add file:// urls to exceptions

--- background.js
+++ background.js
@@ -4,11 +4,27 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     lastOpenedTabId = undefined;
 });
 
+const blacklist = [
+    "chrome://",
+    "view-source:",
+    "file://"
+];
+
 chrome.tabs.onCreated.addListener(function (tab) {
-    if (!tab.url.startsWith("chrome://") && !tab.url.startsWith("view-source:") && tab.openerTabId && tab.active) {
-        chrome.tabs.update(tab.openerTabId, {active: true}, function () {
-            lastOpenedTabId = tab.id;
-        });
+    if (tab.openerTabId && tab.active) {
+        var excluded = false;
+        for (var i in blacklist) {
+            if (blacklist.hasOwnProperty(i)) {
+                if (tab.url.startsWith(blacklist[i])) {
+                    excluded = true;
+                }
+            }
+        }
+        if (!excluded) {
+            chrome.tabs.update(tab.openerTabId, {active: true}, function () {
+                lastOpenedTabId = tab.id;
+            });
+        }
     }
     if (lastOpenedTabId) {
         chrome.tabs.get(lastOpenedTabId, function (lastOpenedTab) {
